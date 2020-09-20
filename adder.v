f0e8da00a0a09aae5acfde9a0e441e1e01c58e1f
rithikrathi19 <39721060+rithikrathi19@users.noreply.github.com>
2020-09-20 16:24:45 +0530

    Add files via upload

---
+++ adder.v
@@ -0,0 +1,12 @@
+//N bit parameterized adder
+module adder #(parameter N=32)(out,in1,in2);
+ input [N-1:0]in1,in2;
+ output reg[N-1:0]out;
+ 
+ always@(*)
+ begin
+	out = in1+in2;
+ end
+ 
+endmodule
+ 
