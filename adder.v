0a3acdccee368b6f7d4f100e890c864969a1a148
rithikrathi19 <rithikrathi19@gmail.com>
2020-10-13 12:25:37 +0530

    changes

--- adder.v
+++ adder.v
@@ -1,11 +1,11 @@
 //N bit parameterized adder
-module adder #(parameter N=32)(out,in1,in2);
- input [N-1:0]in1,in2; //N bit inputs
+module adder #(parameter N=32)(out,ip1,ip2);
+ input [N-1:0]ip1,ip2; //N bit inputs
  output reg[N-1:0]out;
- 
+
  always@(*)
  begin
-	out = in1+in2;
+	out = ip1+ip2;
  end
  
 endmodule
